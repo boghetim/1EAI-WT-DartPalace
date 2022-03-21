467b08ef56c8fbf6b6f89025ca9ebb5eb77be8b5
Tim Boghe <tim.boghe@student.pxl.be>
2022-03-21 15:57:24 +0100

    raw file

---
+++ Dart_Palace.js
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<script language="javascript" type="text/javascript">
+
+function myFunction501()
+{
+  document.getElementById("demo").innerHTML = "501.";
+}
+function myFunction301()
+{
+  document.getElementById("demo").innerHTML = "301.";
+}
+function myFunction101()
+{
+  document.getElementById("demo").innerHTML = "101.";
+}
+
+<button type="button" onclick="myFunction501()">501</button>
+<button type="button" onclick="myFunction301()">301</button>
+<button type="button" onclick="myFunction101()">101</button>
+
+
+
+
+
+
+<button type="button" onclick="myFunction1()">1</button>
+<button type="button" onclick="myFunction2()">2</button>
+<button type="button" onclick="myFunction3()">3</button>
+
+<button type="button" onclick="myFunction4()">4</button>
+<button type="button" onclick="myFunction5()">5</button>
+<button type="button" onclick="myFunction6()">6</button>
+
+<button type="button" onclick="myFunction7()">7</button>
+<button type="button" onclick="myFunction8()">8</button>
+<button type="button" onclick="myFunction9()">9</button>
+
+<button type="button" onclick="myFunctionEnter()">Enter</button>
+<button type="button" onclick="myFunctionNC()">no Score</button>
+
+when(enter).....
+
+
+when(no Score) .....
