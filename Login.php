fadfdc3a51cc62a8966c1ad54eb7dd66c469f49b
tim boghe <tim.boghe@student.pxl.be>
2022-05-23 17:25:16 +0200

    Update Login.php

--- Login.php
+++ Login.php
@@ -18,14 +18,14 @@ error_reporting(E_ALL);
 
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-    
+
 
       $count = mysqli_num_rows($result);
 
       // If result matched $myusername and $mypassword, table row must be 1 row
 
       if($count == 1) {
-         session_register("myusername");
+        
          $_SESSION['login_user'] = $myusername;
 
          header("location: index.html");
