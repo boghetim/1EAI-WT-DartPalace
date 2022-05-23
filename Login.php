68f4ea4cff35388b58d115132c9f60247d0d1da0
tim boghe <tim.boghe@student.pxl.be>
2022-05-23 17:22:06 +0200

    Update Login.php

--- Login.php
+++ Login.php
@@ -18,7 +18,7 @@ error_reporting(E_ALL);
 
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
+    
 
       $count = mysqli_num_rows($result);
 
